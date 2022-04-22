93829d39f992faffa5c0299bafd80c1f21452db3
cmwolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-04-22 15:35:27 -0500

    Built a homepage class, moved the Drivers class to main page and moved the locators that are selected to home page since that's where it's easier to use them

--- homepage.py
+++ homepage.py
@@ -13,39 +13,18 @@ the element is active.
 """
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common import exceptions
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from decorators import Timer, Log
-
-
-class HomePageDropDown:
-    """This class is for locators on the Home Page"""
-    DROP_DOWN_MENU = ".ui-link"
-    DD_BTN_ATTEMPT_1 = "//a[contains(@href, '#menu']"
-    DD_FORETEES_BTN_XPATH = "//span[@class='smartphone-nav-heading'][normalize-space()='Foretees']"
-    DD_BUTTON = "//div[@id='mm-menu-link']"
-    FORETEES_DD_XPATH = "//span[@class='smartphone-nav-heading'][normalize-space()='Foretees']"
-
-
-class HomePageLocators:
-    """This class is for locators on the members home page after logging in"""
-    FORETEES_BUTTON_PARTIAL = "Foretees"
-    FORETEES_BUTTON_XPATH = "//span[text()[normalize-space()='Foretees']]"
-    FORETEES_XPATH_LONG = "//div[@id='textured-cssmenu']/ul[1]/li[5]/a[1]/span[1]"
-    FORETEES_BUTTON_CSS_SELECTOR = "div#textured-cssmenu>ul>li:nth-of-type(5)>a>span"
-    FORETEES_BUTTON_PAGE_ID = "//*[@data-pageid='53']"
-    # Error Message: selenium.common.exceptions.ElementNotInteractableException: Message: element not interactable
-    FORETEES_BUTTON_XPATH_1 = "//span[@class='textured-nav-heading textured-nav-heading-unselected'][normalize-space()='Foretees']"
-    FORETEES_BUTTON_XPATH_2 = "//a/span[contains(@class,'textured-nav-heading textured-nav-heading-unselected')][normalize-space()='Foretees']"
-
+from config.secrets import DROPDOWN_LINK, DROPDOWN_DIV, FORETEES_DROPDOWN_BTN, FORETEES_TOP_BTN
+from config.secrets import CHROMEDRIVER, FIREFOXDRIVER, SAFARIDRIVER, EDGEDRIVER
 
 @Timer()
 @Log(level="TRACE").catch()
 def click_foretees(driver):
-    locator = HomePageLocators.FORETEES_BUTTON_XPATH_2
+    locator = FORETEES_TOP_BTN
     try:
         element = driver.find_element(By.XPATH, locator)
         element.click()
@@ -67,8 +46,8 @@ def click_foretees(driver):
 @Timer()
 @Log(level="TRACE").catch()
 def click_drop_down(driver):
-    locator = HomePageDropDown.DD_BUTTON
-    foretees = HomePageDropDown.FORETEES_DD_XPATH
+    locator = DROPDOWN_DIV
+    foretees = DROPDOWN_LINK
     try:
         element = driver.find_element(By.XPATH, locator)
         print(element)
@@ -87,22 +66,57 @@ def click_drop_down(driver):
 @Log(level="TRACE").catch()
 def home_page(driver):
     try:
-        driver.find_element(By.CSS_SELECTOR, HomePageLocators.DROP_DOWN_MENU).click_and_hold()
+        driver.find_element(By.CSS_SELECTOR, DROPDOWN_DIV).click()
         driver.implicitly_wait(10)
     except exceptions.ElementNotInteractableException as se:
         print(se)
 
-class HomePageLocators:
+
+class HomePage:
     """Locators on Logged in home page"""
 
-    DROP_DOWN_MENU_XPATH = "//div[@id='mm-menu-link']"
+    def __init__(self, url, driver_name="Chrome"):
+        self.url = url
+        if driver_name == "FireFox":
+            self.driver = webdriver.Chrome(FIREFOXDRIVER)
+        elif driver_name == "Edge":
+            self.driver = webdriver.Chrome(EDGEDRIVER)
+        elif driver_name == "Safari":
+            self.driver = webdriver.Chrome(SAFARIDRIVER)
+        elif driver_name == "Chrome":
+            self.driver = webdriver.Chrome(CHROMEDRIVER)
 
-    def activate_page(self, driver, locator):
-        wait = WebDriverWait(60).
-        pass
+    def __repr__(self):
+        return f"Browser Object Created: Using {self.driver} to open {self.url}"
 
-    def wait_for_element(self, driver:webdriver, locator:tuple, wait:int=60):
+    @Timer()
+    @Log()
+    def wait_for_element(self, locator:tuple, wait:int=60):
         """Wait for element to show up on page"""
         try:
+            driver = self.driver
+            driver.maximize_window()
+            driver.get(self.url)
             element = WebDriverWait(driver, wait).until(EC.presence_of_element_located(locator))
+            text = element.text
+            if text:
+                print(text)
+                element.click()
 
+            else:
+                raise exceptions.NoSuchElementException
+
+        except exceptions.ElementNotInteractableException as ni:
+            print(f"Exception: {ni}")
+
+        except exceptions.NoSuchElementException as ne:
+            print(f"Exception: {ne}")
+
+        except exceptions.ElementNotVisibleException as nv:
+            print(f"Exception: {nv}")
+
+        except AttributeError as ae:
+            print(f"Error: {ae}")
+
+        finally:
+            self.driver.implicitly_wait(wait / 3)
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from config.secrets import COURSE_URL
 from login import login
 from selenium import webdriver
-from homepage import click_foretees, click_drop_down, home_page
+from homepage import HomePage
+from config.secrets import CHROMEDRIVER, FIREFOXDRIVER, SAFARIDRIVER, EDGEDRIVER
+from config.secrets import DROPDOWN_LINK, DROPDOWN_DIV, FORETEES_DROPDOWN_BTN, FORETEES_TOP_BTN
+import time
 
-# Path to Chrome Driver
-CHROMEDRIVER_PATH = r"C:\\webdriver\\chromedriver.exe"
-FIREFOXDRIVER_PATH = r"C:\\webdriver\\geckodriver.exe"
 
+class Drivers(webdriver):
+
+    def __init__(self, url, driver_name="Chrome"):
+        self.url = url
+        if driver_name == "FireFox":
+            self.driver = webdriver.Chrome(FIREFOXDRIVER)
+        elif driver_name == "Edge":
+            self.driver = webdriver.Chrome(EDGEDRIVER)
+        elif driver_name == "Safari":
+            self.driver = webdriver.Chrome(SAFARIDRIVER)
+        elif driver_name == "Chrome":
+            self.driver = webdriver.Chrome(CHROMEDRIVER)
+
+    def __repr__(self):
+        return f"Browser Object Created: Using {self.driver} to open {self.url}"
+
+# # Path to Chrome Driver
+# CHROMEDRIVER_PATH = r"C:\\webdriver\\chromedriver.exe"
+# FIREFOXDRIVER_PATH = r"C:\\webdriver\\geckodriver.exe"
+
+
+driver = Drivers(COURSE_URL)
+
+
+homepage = HomePage
 
 if __name__ == '__main__':
-    # Create a webdriver instance
-    driver = webdriver.Chrome(CHROMEDRIVER_PATH)
+    driver = Drivers(COURSE_URL)
 
     # Open the url with driver
     driver.get(COURSE_URL)
@@ -22,12 +46,9 @@ if __name__ == '__main__':
     # run login function
     login(driver)
 
-    # wait for page to load
-    driver.implicitly_wait(10)
-
-    # run home_page function
-    # click_foretees(driver)
-    click_drop_down(driver)
+    # Run HomePage Method
+    homepage.wait_for_element(FORETEES_TOP_BTN)
     # ================================================================ #
+    time.sleep(10)
     # close Browser
     driver.close()
