a3b494c7f363aec0600d05a3ab4dc233f4e66a7d
cmwolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-05-01 15:43:56 -0500

    Homepage Tests created the class

---
+++ tests/main_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestHomePage(unittest.TestCase):
+    """This class is to run tests on the homepage class to ensure it is working"""
+
+    def test_homepage(self):
+        """Ensure the correct homepage is accessed"""
+
+        pass
